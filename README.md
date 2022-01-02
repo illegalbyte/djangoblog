3edea903e838ca65651c69b756c21165f87c61a0
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-02 14:21:46 +1100

    Add more unit tests

--- README.md
+++ README.md
@@ -16,8 +16,8 @@ A list of existing and planned functionality for my DjangoBlog project.
 - [X] Edit existing posts
 - [X] Hide the edit functionality for non-authenticated users
 - [X] Add tests for requesting index page
-- [ ] Add tests for requesting an individual post's page
-- [ ] Add tests for submitting an edit
+- [X] Add tests for requesting an individual post's page
+- [X] Add tests for submitting an edit
 - [ ] Add tests for posting via webform
 - [X] CI/CD using GitHub Actions
 - [ ] Function to create draft posts
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.http import response
 from django.contrib.auth.models import AnonymousUser, User
 from django.utils import timezone
+from django.apps import apps
 
 from .models import Post
 
@@ -37,7 +38,7 @@ class IndexPageTests(TestCase):
         create_post('Test title', 'Test text')
         response = self.client.get('/')
         self.assertContains(response, 'Test title')
-        
+
 
 class PostDetailPageTests(TestCase):
 
@@ -49,7 +50,6 @@ class PostDetailPageTests(TestCase):
         response = self.client.get('/post/1/')
         self.assertEqual(response.status_code, 200)
 
-# Test for requesting the post edit page
     def test_post_edit_page_reachable(self):
         '''
         Make sure the post edit page is reachable
@@ -58,10 +58,84 @@ class PostDetailPageTests(TestCase):
         response = self.client.get('/post/1/edit/')
         self.assertEqual(response.status_code, 200)
 
-# Test for creating a new post
+    def test_post_edit_page_contains_form(self):
+        '''
+        Make sure the post edit page contains the form
+        '''
+        create_post('Test title', 'Test text')
+        response = self.client.get('/post/1/edit/')
+        self.assertContains(response, '<form')
 
-# Test for editing an existing post
+class PostEditPageTests(TestCase):
+    def setUp(self):
+        self.client.force_login(User.objects.get_or_create(username='testeditor')[0])
+    
+    def test_post_edit_page_reachable(self):
+        '''
+        Make sure the post edit page is reachable
+        '''
+        create_post('Test title', 'Test text')
+        response = self.client.get('/post/1/edit/')
+        self.assertEqual(response.status_code, 200)
 
+    def test_post_edit_page_contains_form(self):
+        '''
+        Make sure the post edit page contains the form
+        '''
+        create_post('Test title', 'Test text')
+        response = self.client.get('/post/1/edit/')
+        self.assertContains(response, '<form')
+
+    def test_post_edit_page_contains_title(self):
+        '''
+        Make sure the post edit page contains the title
+        '''
+        create_post('Test title', 'Test text')
+        response = self.client.get('/post/1/edit/')
+        self.assertContains(response, 'Test title')
+
+    def test_post_edit_page_contains_text(self):
+        '''
+        Make sure the post edit page contains the text
+        '''
+        create_post('Test title', 'Test text')
+        response = self.client.get('/post/1/edit/')
+        self.assertContains(response, 'Test text')
+
+
+class BlogDataTestCase(TestCase):
+    def setUp(self):
+
+        # User
+        self.user = User.objects.create_superuser(
+            username='admin',
+            email='admin@hellogithub.com',
+            password='admin'
+        )
+        self.client.login(username='admin', password='admin')
+
+        # publish test article
+        self.post1 = Post.objects.create(
+            author=self.user,
+            title='Test title one',
+            text='Test content one',
+        )
+        self.post1.publish()
+
+    def test_editing_a_post_redirects_to_post_detail_page(self):
+        '''
+        Test that editing a post redirects to the post detail page
+        '''
+        response = self.client.post('/post/1/edit/', {'title': 'Edited title', 'text': 'Edited text', 'author': self.user})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/post/1/')
+
+    def test_editing_a_post_changes_the_title(self):
+        '''
+        Test that editing a post changes the title
+        '''
+        self.client.post('/post/1/edit/', {'title': 'This title has changed', 'text': 'Edited text', 'author': self.user})
+        self.assertEqual(Post.objects.get(pk=1).title, 'This title has changed')
 # Test for deleting an existing post
 
 # Test for creating a new comment
