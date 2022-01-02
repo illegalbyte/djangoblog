a1b2ab424f97a324fe76b38e4bc28294578be98e
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-03 01:40:43 +1100

    Implement comments

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Post
+from .models import Comment, Post
 
 class PostForm(forms.ModelForm):
 
@@ -8,3 +8,8 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
+class CommentForm(forms.ModelForm):
+    
+        class Meta:
+            model = Comment
+            fields = ('author', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 # Create your views here.
 
@@ -39,3 +39,16 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
